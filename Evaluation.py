dd7ebf95ffae20c2eb7b99f024d684ce6ecd5074
sebastianmorta <morta.sebastian@gmail.com>
2024-01-08 13:20:39 +0100

    init

---
+++ Evaluation.py
@@ -0,0 +1,41 @@
+import torch
+from datasets import load_from_disk, load_metric
+from transformers import AutoModel, AutoTokenizer, Trainer, TrainingArguments, AutoModelForAudioClassification, \
+    AutoModelForTokenClassification, BertConfig, AutoConfig, BertModel
+
+import AudioTextNER
+import TextNER
+
+from AudioTextNER import CustomAudioTextModel
+from Ontonotes import Ontonotes5Features
+from TextNER import CustomTextModel
+# from modelsClass import DatasetHelper, process_voxpopuli_non_normalized, evaluate_model, Ontonotes5Features
+
+num_labels = len(Ontonotes5Features.ontonotes_labels_bio)
+dataset = load_from_disk('./dataset/DatasetVoxpopuliAfterMap10p')
+text_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+textConfig = AutoConfig.from_pretrained('./resultPretrained/TextModelsmall1')
+
+modelText = CustomTextModel(textConfig, num_labels=num_labels).to("mps")
+modelText.load_state_dict(torch.load('./resulttorch/TextModelsmall.pth'))
+
+audio_text_config_text = BertConfig.from_pretrained("bert-base-uncased")
+audio_text_config_audio = AutoConfig.from_pretrained("facebook/wav2vec2-base-960h")
+
+modelAudioText = CustomAudioTextModel(audio_config=audio_text_config_audio, text_config=audio_text_config_text,
+                                      num_labels=num_labels)
+modelAudioText.load_state_dict(torch.load('./resulttorch/AudioTextModel.pth'))
+
+# models = [modelText, modelAudioText]
+models = [modelAudioText, modelText]
+# evaluators = [TextModelUtils.evaluate_text_model, AudioTextModelUtils.evaluate_audio_text_model]
+evaluators = [AudioTextNER.evaluate_audio_text_model, TextNER.evaluate_text_model]
+
+if __name__ == "__main__":
+    for model, evaluator in zip(models, evaluators):
+
+        model.eval()
+        print(dataset['validation'].shape)
+        result = evaluator(dataset['validation'], text_tokenizer, model)
+        print("RESULTSS", result)
---
+++ TextNER.py
@@ -0,0 +1,87 @@
+from tqdm import tqdm
+from transformers import BertPreTrainedModel, BertModel, AutoConfig, PreTrainedModel, AutoModel, \
+    AutoModelForTokenClassification
+import torch
+import torch.nn as nn
+from transformers.modeling_outputs import SequenceClassifierOutput
+from Ontonotes import Ontonotes5Features
+
+
+class CustomTextModel(BertPreTrainedModel):
+    config_class = AutoConfig
+    def __init__(
+            self,
+            config,
+            num_labels
+    ):
+        super(CustomTextModel, self).__init__(config=config)
+        self.model_name = 'TextModel'
+        self.bert = AutoModel.from_config(config)
+        self.classifier = nn.Linear(config.hidden_size, num_labels)
+        self.num_labels = num_labels
+        self.text_dim = config.hidden_size
+
+    def forward(
+            self,
+            input_ids,
+            attention_mask=None,
+            labels=None
+    ):
+        text_output = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+         ).pooler_output
+
+        logits = self.classifier(text_output)
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            logits = logits.view(-1, self.num_labels)
+            if len(labels) % logits.shape[0] == 0:
+                labels = labels.view(logits.shape[0], -1)
+                labels = labels[:, 0]
+                loss = loss_fct(logits, labels)
+            else:
+                raise ValueError("Liczba etykiet nie jest podzielna przez rozmiar wsadu")
+
+        return SequenceClassifierOutput(loss=loss, logits=logits)
+
+def evaluate_text_model(dataset, tokenizer, model):
+    true_y = []
+    pred_y = []
+    for item in tqdm(dataset):
+        tokenized_item = tokenize_adjust_labels(item, tokenizer)
+        gold = tokenized_item['labels']
+        del tokenized_item['labels']
+
+        with torch.no_grad():
+            output = model(**tokenized_item)
+            logits = output.logits.to('mps')
+            predictions = torch.argmax(logits, dim=0)
+
+        true_y.append(gold[1:-1])
+        pred_y.append(predictions.tolist()[1:-1])
+    return [true_y, pred_y]
+
+def tokenize_adjust_labels(example, tokenizer):
+    tokenized_samples = tokenizer.encode_plus(example['tokens'], is_split_into_words=True, return_tensors="pt").to(
+        "mps")
+    tokenized_samples.pop("token_type_ids", None)
+    prev_wid = -1
+    word_ids_list = tokenized_samples.word_ids()
+    existing_label_ids = example["tags"]
+    i = -1
+    adjusted_label_ids = []
+
+    for wid in word_ids_list:
+        if (wid is None):
+            adjusted_label_ids.append(-100)
+        elif (wid != prev_wid):
+            i = i + 1
+            adjusted_label_ids.append(existing_label_ids[i])
+            prev_wid = wid
+        else:
+            adjusted_label_ids.append(existing_label_ids[i])
+
+    tokenized_samples["labels"] = adjusted_label_ids
+    return tokenized_samples
